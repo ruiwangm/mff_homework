7bc1aac542a0b42125f9ec068a255f396125dfea
Rui <grashearts@gmail.com>
2015-10-13 21:30:29 +0200

    another needed function

---
+++ singleYahooStructure2table.m
@@ -0,0 +1,17 @@
+function t = singleYahooStructure2table( yahooStruct )
+%
+% Input:
+%   yahooStruct     single structure as returned by hist_stock_data
+%
+% Output:
+%   t           table with Dates in first column, adjusted closing prices
+%               in second column and name of second column equals ticker
+%               symbol
+
+varname = createValidName(yahooStruct.Ticker);
+
+t = table(yahooStruct.Date, yahooStruct.AdjClose, ...
+    'VariableNames', {'Dates', varname});
+
+end
+
